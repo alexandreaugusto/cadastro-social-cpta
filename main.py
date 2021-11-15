cfc49a6f8dbdbee6939c3c0d569be4c18ea70a0e
Alexandre A. D. de Oliveira <alexandreaddeoliveira@gmail.com>
2021-11-15 03:28:59 -0300

    Login funcional

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ app=Flask(__name__)
 app.secret_key = 'your secret key'
 
 # Enter your database connection details below
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'cadastro_cpta'
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_PORT']=3308
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='cadastro_socioeconomico'
 
 # Intialize MySQL
 mysql = MySQL(app)
 
-# http://localhost:5000/pythonlogin/ - this will be the login page, we need to use both GET and POST requests
 @app.route('/cadastro-cpta/', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
@@ -29,7 +29,7 @@ def login():
         password = request.form['password']
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        cursor.execute('SELECT * FROM accounts WHERE email = %s AND senha = MD5(%s)', (username, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
         # If account exists in accounts table in out database
@@ -37,7 +37,8 @@ def login():
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['id'] = account['id']
-            session['username'] = account['username']
+            session['nome'] = account['nome']
+            session['email'] = account['email']
             # Redirect to home page
             return 'Logged in successfully!'
         else:
@@ -47,7 +48,6 @@ def login():
     return render_template('index.html', msg=msg)
 
 
-# http://localhost:5000/python/logout - this will be the logout page
 @app.route('/cadastro-cpta/logout')
 def logout():
     # Remove session data, this will log the user out
